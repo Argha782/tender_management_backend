dd0dbd16e3a73997ba2b5201bd389b9e7ffc657d
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-05-27 12:23:01 +0530

    case mistake: asyncHandler to asynchandler file name correction

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asynchandler.js";
 import { sendEmail } from "../utils/sendEmail.js";
 import { sendToken } from "../utils/sendToken.js";
 import twilio from "twilio";
