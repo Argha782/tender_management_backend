2ff778f63150714ccbf1a8743e1b078d09b399dd
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-11-19 14:41:39 +0530

    updated cors 3 19-11-25

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const allowedOrigins = [
   "http://localhost:5173", // React local dev
   "https://tender-management-frontend-three.vercel.app",
   "https://tender-management-frontend-argha-sahas-projects.vercel.app",
-  "https://tender-management-frontend-mdq47xtsj-argha-sahas-projects.vercel.app", // your deployed frontend
+  "https://tender-management-frontend-6zs9bua0t-argha-sahas-projects.vercel.app", // your deployed frontend
 ];
 
 
